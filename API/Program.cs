b55be64f71d6ca7a92eced1cbd418c073b92bf6a
Till Nitsche <till.nitsche@web.de>
2021-10-05 16:13:59 +0200

    added front-end

--- API/Program.cs
+++ API/Program.cs
@@ -15,12 +15,11 @@ namespace API
         {
             CreateHostBuilder(args).Build().Run();
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-    }
+    } 
 }
